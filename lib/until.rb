e555042d377e6cdbde31af59a56db9d05c850789
Julian George <jules104@gmail.com>
2019-07-08 06:04:57 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,11 @@
 def using_until
   levitation_force = 6
-  
+  while levitation_force < 10
+  puts 'Wingardium Leviosa'
+  levitation_force += 1
 
 end
+
+#using_until puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10
+     #Failure/Error: expect{ using_until }.to output(looping_string).to_stdout
+  
